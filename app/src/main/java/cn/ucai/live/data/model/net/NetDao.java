272e8e5527c4efa4c064d94082eeed6d4ce64640
xudengqiao <1257678683@qq.com>
2017-02-27 16:32:19 +0800

    NetDao的添加及OnCompleteListener接口的添加

---
+++ app/src/main/java/cn/ucai/live/data/model/net/NetDao.java
@@ -0,0 +1,139 @@
+package cn.ucai.live.data.model.net;
+
+import android.content.Context;
+
+import com.hyphenate.chat.EMGroup;
+
+import java.io.File;
+
+import cn.ucai.live.I;
+import cn.ucai.live.utils.MD5;
+import cn.ucai.live.utils.OkHttpUtils;
+
+
+/**
+ * Created by Administrator on 2017/2/8 0008.
+ */
+
+public class NetDao {
+    public static void register(Context context,String username,String nickname,String password,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nickname)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .post()
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void unRegister(Context context, String username, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void login(Context context, String username, String password, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void findUserByUsername(Context context,String username,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void updateUserNick(Context context,String username,String usernick, OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,usernick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void updateAvatar(Context context, String username, File file,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    public static void addContact(Context context,String username,String cname,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_ADD_CONTACT)
+                .addParam(I.Contact.USER_NAME,username)
+                .addParam(I.Contact.CU_NAME,cname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void loadContactList(Context context,String username,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void deleteContact(Context context,String username ,String cname,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DELETE_CONTACT)
+                .addParam(I.Contact.USER_NAME,username)
+                .addParam(I.Contact.CU_NAME,cname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void createGroup(Context context, EMGroup group, File file, OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
+                .addParam(I.Group.HX_ID,group.getGroupId())
+                .addParam(I.Group.NAME,group.getGroupName())
+                .addParam(I.Group.DESCRIPTION,group.getDescription())
+                .addParam(I.Group.OWNER,group.getOwner())
+                .addParam(I.Group.IS_PUBLIC,String.valueOf(group.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES,String.valueOf(group.isAllowInvites()))
+                .targetClass(String.class)
+                .addFile2(file)
+                .post()
+                .execute(listener);
+    }
+
+    public static void addGroupMembers(Context context, String members, String groupId, OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_ADD_GROUP_MEMBERS)
+                .addParam(I.Member.USER_NAME,members)
+                .addParam(I.Member.GROUP_HX_ID,groupId)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+
+    public static void updateGroupName(Context context, String groupId, String groupname, OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_GROUP_NAME)
+                .addParam(I.Group.GROUP_ID,groupId)
+                .addParam(I.Group.NAME,groupname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void deleteGroup(Context context,String groupHxid,OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DELETE_GROUP_BY_HXID)
+                .addParam(I.Group.HX_ID,groupHxid)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+}
